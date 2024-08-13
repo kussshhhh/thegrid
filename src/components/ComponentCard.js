0f89f99ad090f929529d9c38f68341b37ab4478a
kussshhhh <kushsep21@gmail.com>
2024-08-14 01:10:07 +0530

    hello

--- src/components/ComponentCard.js
+++ src/components/ComponentCard.js
@@ -2,31 +2,12 @@ import React from 'react';
 
 function ComponentCard({ component }) {
   const ComponentToRender = component.component;
-  const iframeContent = `
-    <html>
-      <head>
-        <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
-        <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
-        <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-      </head>
-      <body>
-        <div id="root"></div>
-        <script type="text/babel">
-          const ComponentToRender = ${ComponentToRender.toString()};
-          ReactDOM.render(<ComponentToRender />, document.getElementById('root'));
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div className='component-card'>
-      <iframe
-        srcDoc={iframeContent}
-        title={component.name}
-        className='component-preview'
-        sandbox="allow-scripts"
-      />
+      <div className='component-preview'>
+        <ComponentToRender />
+      </div>
       <h3 className='component-name'>{component.name}</h3>
       <p className='component-description'>{component.description}</p>
       <div className='component-meta'>
@@ -39,5 +20,3 @@ function ComponentCard({ component }) {
 }
 
 export default ComponentCard;
-
-
